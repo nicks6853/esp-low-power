74eb9c79301960b0cb7f233e133b19ba01b26ab5
nicks6853 <nicks6853@gmail.com>
2026-02-22 16:13:29 -0400

    Update edge device to publish temperature and humidity periodically

--- src/edge/main.cpp
+++ src/edge/main.cpp
@@ -12,44 +12,52 @@
 
 BME280I2C bme;
 unsigned long lastReading = 0;
+EspNowChunker chunker;
 
 uint8_t destinationMac[] = {0xD4, 0x8A, 0xFC, 0xA8, 0xA3, 0x24};
 
 /**
  * @brief read temperature from the connected BME280
  */
-// void readTemperature() {
-//     float temperature = bme.temp();
-//     Serial.printf("Temperature read: %.2f°C\n", temperature);
-//
-//     HAStateUpdate<float> state;
-//
-//     const char* stateTopic = "multisensor/temp/state";
-//     strncpy(state.topic, stateTopic, sizeof(state.topic));
-//     state.topic[sizeof(state.topic) - 1] = '\0';
-//
-//     state.value = temperature;
-//     state.messageType = MessageType::STATE_UPDATE_FLOAT;
-//
-//     // homeAssistant.publishState(state);
-// }
+void sendTemperature() {
+    float temperature = bme.temp();
+    Serial.printf("Temperature read: %.2f°C\n", temperature);
 
-// void readHumidity() {
-//     float humidity = bme.hum();
-//     Serial.printf("Humidity read: %.2f%%\n", humidity);
-//
-//     HAStateUpdate<float> state;
-//
-//     const char* stateTopic = "multisensor/hum/state";
-//     strncpy(state.topic, stateTopic, sizeof(state.topic));
-//     state.topic[sizeof(state.topic) - 1] = '\0';
-//
-//     state.value = humidity;
-//     state.messageType = MessageType::STATE_UPDATE_FLOAT;
-//
-//     // homeAssistant.publishState(state);
-// }
-//
+    HAStateUpdate<float> state;
+
+    const char* stateTopic = "multisensor/temp/state";
+    strncpy(state.topic, stateTopic, sizeof(state.topic));
+    state.topic[sizeof(state.topic) - 1] = '\0';
+
+    state.value = temperature;
+
+    HAMessage* msg = new HAMessage;
+    msg->messageType = MessageType::STATE_UPDATE_FLOAT;
+
+    msg->payload.stateUpdateF = state;
+
+    chunker.send(destinationMac, (uint8_t*)msg, sizeof(HAMessage));
+}
+
+void sendHumidity() {
+    float humidity = bme.hum();
+    Serial.printf("Humidity read: %.2f%%\n", humidity);
+
+    HAStateUpdate<float> state;
+
+    const char* stateTopic = "multisensor/hum/state";
+    strncpy(state.topic, stateTopic, sizeof(state.topic));
+    state.topic[sizeof(state.topic) - 1] = '\0';
+
+    state.value = humidity;
+
+    HAMessage* msg = new HAMessage;
+    msg->messageType = MessageType::STATE_UPDATE_FLOAT;
+
+    msg->payload.stateUpdateF = state;
+
+    chunker.send(destinationMac, (uint8_t*)msg, sizeof(HAMessage));
+}
 
 void createDevice() {
     Serial.println("Adding device");
@@ -100,7 +108,6 @@ void createDevice() {
     msg->messageType = MessageType::DISCOVERY_PAYLOAD;
     msg->payload.discovery = discoveryPayload;
 
-    EspNowChunker chunker;
     chunker.send(destinationMac, (uint8_t*)msg, sizeof(*msg));
 
     delete device;
@@ -109,36 +116,36 @@ void createDevice() {
 }
 
 void sendCallback(uint8_t* mac __attribute__((unused)), uint8_t sendStatus) {
-    Serial.printf("Last message sent status: %d", sendStatus);
+    LOG(Serial.printf("Last message sent status: %d\n", sendStatus));
 }
 
 uint8_t initializeEspNow() {
-    Serial.println("Initializing ESPNOW...");
+    LOG(Serial.println("Initializing ESPNOW..."));
 
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
 
     // Print mac address
-    Serial.print("Mac address: ");
-    Serial.println(WiFi.macAddress());
+    LOG(Serial.print("Mac address: "));
+    LOG(Serial.println(WiFi.macAddress()));
 
     if (esp_now_init() != 0) {
-        Serial.println("Error initializing ESPNOW!");
+        LOG(Serial.println("Error initializing ESPNOW!"));
         return 0;
     }
 
     // Set the current role
     uint8_t res = esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
-    Serial.printf("RESULT FROM SET ROLE: %d", res);
+    LOG(Serial.printf("RESULT FROM SET ROLE: %d", res));
     // Register peer
     res = esp_now_add_peer(destinationMac, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
-    Serial.printf("RESULT FROM ADD PEER: %d", res);
+    LOG(Serial.printf("RESULT FROM ADD PEER: %d", res));
 
     // Register callback on send
     res = esp_now_register_send_cb(sendCallback);
-    Serial.printf("Result from register send callback: %d", res);
+    LOG(Serial.printf("Result from register send callback: %d", res));
 
-    Serial.println("ESPNOW Initialized!");
+    LOG(Serial.println("ESPNOW Initialized!"));
     return 1;
 }
 
@@ -158,15 +165,10 @@ void loop() {
 
     unsigned long currentTime = millis();
 
-    if (currentTime - lastReading >= 2000) {
-        // Send a message over ESPNOW
-
-        // float temperature = bme.temp();
-        // float humidity = bme.hum();
-
-        // Serial.printf("Sensor read. Temperature: %.2f, Humidity: %.2f",
-        //               temperature, humidity);
-
+    if (currentTime - lastReading >= 5000) {
+        // Send temperature and humidity over ESPNOW
+        sendTemperature();
+        sendHumidity();
         lastReading = currentTime;
     }
 }
